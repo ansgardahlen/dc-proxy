e84d8329a2f186a27ce266ab16c676201cd1772f
Ansgar Dahlen <ansgar.dahlen@googlemail.com>
2019-04-27 15:08:15 +0200

    added condition to create folder

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #
 
+if [ ! -d "$DIRECTORY" ]; then
+    # Will enter here if $DIRECTORY exists, even if it contains spaces
+mkdir -p data/proxy/templates/
+fi
+
 # Download the latest version of nginx.tmpl
 curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > data/proxy/templates/nginx.tmpl
 
